8ce136ce21cbc7439a66b0359af96a136ee8faa8
dilarakiraz <dilarakiraz12345@gmail.com>
2023-09-16 00:49:24 +0300

    add to note

--- app/src/main/java/com/dilarakiraz/upschooltodoo/ui/dailynotes/DailyNotesAdapter.kt
+++ app/src/main/java/com/dilarakiraz/upschooltodoo/ui/dailynotes/DailyNotesAdapter.kt
@@ -7,29 +7,43 @@ import com.dilarakiraz.upschooltodoo.data.model.Note
 import com.dilarakiraz.upschooltodoo.databinding.ItemDailyNoteBinding
 
 class DailyNotesAdapter(
-
+    private val onNoteClick: (String) -> Unit
 ):RecyclerView.Adapter<DailyNotesAdapter.DailyNoteViewHolder>(){
 
     private val noteList = mutableListOf<Note>()
 
     override fun onCreateViewHolder(parent:ViewGroup,viewType:Int):DailyNoteViewHolder {
         val binding = ItemDailyNoteBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return DailyNoteViewHolder(binding)
+        return DailyNoteViewHolder(binding,onNoteClick)
     }
 
     override fun onBindViewHolder(holder: DailyNoteViewHolder,position: Int){
         holder.bind(noteList[position])
     }
 
-    class DailyNoteViewHolder(private val binding: ItemDailyNoteBinding):RecyclerView.ViewHolder(binding.root){
+    class DailyNoteViewHolder(
+        private val binding: ItemDailyNoteBinding,
+        private val onNoteClick: (String) -> Unit
+    ):RecyclerView.ViewHolder(binding.root){
         fun bind(note:Note){
             with(binding){
+                tvTitle.text = note.title
+                tvDesc.text=note.description
 
+                root.setOnClickListener{
+                    onNoteClick(note.description)
+                }
             }
         }
     }
 
     override fun getItemCount(): Int {
         return noteList.size
+    }
+
+    fun updateList(list:List<Note>){
+        noteList.clear()
+        noteList.addAll(list)
+        notifyItemRangeChanged(0,list.size)
     }
 }
--- app/src/main/java/com/dilarakiraz/upschooltodoo/ui/dailynotes/DailyNotesFragment.kt
+++ app/src/main/java/com/dilarakiraz/upschooltodoo/ui/dailynotes/DailyNotesFragment.kt
@@ -1,19 +1,61 @@
 package com.dilarakiraz.upschooltodoo.ui.dailynotes
 
 
+
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.dilarakiraz.upschooltodoo.R
 import com.dilarakiraz.upschooltodoo.common.viewBinding
+import com.dilarakiraz.upschooltodoo.data.source.Database
+import com.dilarakiraz.upschooltodoo.databinding.DialogAddNoteBinding
 import com.dilarakiraz.upschooltodoo.databinding.FragmentDailyNotesBinding
 
 class DailyNotesFragment : Fragment(R.layout.fragment_daily_notes) {
 
     private val binding by viewBinding (FragmentDailyNotesBinding::bind)
 
+    private val dailyNotesAdapter =  DailyNotesAdapter(onNoteClick = ::onNoteClick)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        with(binding){
+            rvDailyNotes.adapter = dailyNotesAdapter
+            dailyNotesAdapter.updateList(Database.getDailyNotes())
+
+            fabAdd.setOnClickListener {
+                showAddDialog()
+            }
+        }
+    }
+    private fun onNoteClick(desc: String) {
+        Toast.makeText(requireContext(), desc, Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun showAddDialog(){
+        val builder = AlertDialog.Builder(requireContext())
+        val dialogBinding = DialogAddNoteBinding.inflate(layoutInflater)
+        builder.setView(dialogBinding.root)
+        val dialog = builder.create()
+
+        with(dialogBinding){
+            btnAddNote.setOnClickListener {
+                val title = etTitle.text.toString()
+                val desc = etDesc.text.toString()
+
+                if (title.isNotEmpty() && desc.isNotEmpty()) {
+                    Database.addDailyNote(title, desc)
+                    dailyNotesAdapter.updateList(Database.getDailyNotes())
+                    dialog.dismiss()
+                } else {
+                    Toast.makeText(requireContext(), "Please fill in the blanks!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        dialog.show()
     }
 }
